841ec53586922f0842b5ec40dc11068443e27a54
root <root@shell.zah.fi>
2014-07-12 01:59:06 +0300

    Small changes

--- main.py
+++ main.py
@@ -2,8 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from sys import argv
+from os import fork
 
 from server import LyliServer
+
+pid = fork()
+if pid > 0:
+    print 'PID: %d' % pid
+    exit(0)
+elif pid < 0:
+    print 'ERROR'
+    exit(1)
 
 server = LyliServer()
 
